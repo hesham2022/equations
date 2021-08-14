07f81799f999aee6aed664e189d42d1c5dd8e3d1
Hesham <hesham2907@gmail.com>
2021-08-13 19:01:59 -0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:cron/cron.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 
@@ -15,63 +16,72 @@ void main() {
 
 class Repository {
   List<Equation> equations = [];
+  void SchedulingTask(Function(Equation equation) callBack) {
+    equations.forEach((e) {
+      if (e.time.second == DateTime.now().second) {
+        callBack(e);
+        // service.sendData({'equation': e.toJson()});
+
+      }
+    });
+  }
 }
 
-final getListKey = 'getList';
+const getListKey = 'getList';
+const addKey = 'add';
 void onStart() {
-  List<Equation> equations = [];
+  final repository = Repository();
 
   WidgetsFlutterBinding.ensureInitialized();
   final service = FlutterBackgroundService();
 
   service.onDataReceived.listen((event) {
-    if (event!["action"] == "setAsForeground") {
-      service.setForegroundMode(true);
-      return;
-    }
+    // if (event!["action"] == "setAsForeground") {
+    //   service.setForegroundMode(true);
+    //   return;
+    // }
 
-    if (event["action"] == "setAsBackground") {
-      service.setForegroundMode(false);
-    }
+    // if (event["action"] == "setAsBackground") {
+    //   service.setForegroundMode(false);
+    // }
 
-    if (event["action"] == "stopService") {
-      service.stopBackgroundService();
-    }
-    if (event["action"] == "add") { 
-       equations.add(Equation.fromJson(event['equation']));
-          service.sendData({
-            getListKey: json.encode(equations.map((e) => e.toJson()).toList())
-          });
-    
-    }
+    // if (event["action"] == "stopService") {
+    //   service.stopBackgroundService();
+    // }
 
-    if (event["action"] == getListKey) {
-      service.sendData(
-          {getListKey: json.encode(equations.map((e) => e.toJson()).toList())});
+    switch (event!["action"]) {
+      case addKey:
+        repository.equations.add(Equation.fromJson(event['equation']));
+        service.sendData({
+          getListKey:
+              json.encode(repository.equations.map((e) => e.toJson()).toList())
+        });
+        break;
+      case getListKey:
+        service.sendData({
+          getListKey:
+              json.encode(repository.equations.map((e) => e.toJson()).toList())
+        });
+        break;
     }
+ 
   });
 
   // bring to foreground
-  service.setForegroundMode(true);
+  service.setForegroundMode(false);
   Timer.periodic(Duration(seconds: 1), (timer) async {
-    if (equations.length > 0) {
-      equations.forEach((e) {
-        if (e.time.second == DateTime.now().second) {
-          // service.sendData({'equation': e.toJson()});
-          service.setNotificationInfo(
-              title: 'solved',
-              content:
-                  '${e.firstOpeartor} ${e.opeartor} ${e.secondOpeartor} = ${e.result}');
-          // equations.remove(e);
+    repository.SchedulingTask((e) {
+      service.setNotificationInfo(
+          title: 'solved',
+          content:
+              '${e.firstOpeartor} ${e.opeartor} ${e.secondOpeartor} = ${e.result}');
 
-          e.completed = true;
-          service.sendData({
-            getListKey: json.encode(equations.map((e) => e.toJson()).toList())
-          });
-        }
+      e.completed = true;
+      service.sendData({
+        getListKey:
+            json.encode(repository.equations.map((e) => e.toJson()).toList())
       });
-    }
-
+    });
     if (!(await service.isServiceRunning())) timer.cancel();
   });
 }
@@ -98,22 +108,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            // ElevatedButton(
-            //   child: Text("Show completed"),
-            //   onPressed: () {
-            //     FlutterBackgroundService().sendData({
-            //       "action": completedKey,
-            //     });
-            //   },
-            // ),
-            // ElevatedButton(
-            //   child: Text("Show unompleted"),
-            //   onPressed: () {
-            //     FlutterBackgroundService().sendData({
-            //       "action": unCompletedKey,
-            //     });
-            //   },
-            // ),
+   
             StreamBuilder<Map<String, dynamic>?>(
               stream: FlutterBackgroundService().onDataReceived,
               builder: (context, snapshot) {
@@ -131,15 +126,7 @@ class _MyAppState extends State<MyApp> {
                           .map((e) => Equation.fromJson(e))
                           .toList();
                   return Column(children: [
-                    
-    //  ElevatedButton(
-    //           child: Text("Show unompleted"),
-    //           onPressed: () {
-    //             FlutterBackgroundService().sendData({
-    //               "action": unCompletedKey,
-    //             });
-    //           },
-    //         ),
+                
 
                     Text('Completed'),
                     for (final e in list.where((element) => element.completed))
@@ -153,8 +140,9 @@ class _MyAppState extends State<MyApp> {
                           Text('${e.result}'),
                         ],
                       ),
-                          Text('unCompleted'),
-                    for (final e in list.where((element) => element.completed==false))
+                    Text('unCompleted'),
+                    for (final e
+                        in list.where((element) => element.completed == false))
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
@@ -168,54 +156,20 @@ class _MyAppState extends State<MyApp> {
                   ]);
                 }
 
-             
                 return Text('com');
               },
             ),
-            //   StreamBuilder<Map<String, dynamic>?>(
-            //   stream: FlutterBackgroundService().onDataReceived,
-            //   builder: (context, snapshot) {
-            //     if (!snapshot.hasData) {
-            //       return Center(
-            //         child: CircularProgressIndicator(),
-            //       );
-            //     }
-
-            //     final data = snapshot.data!;
-
-            //     if (data['unCompleted'] != null) {
-            //       final List<Equation> list =
-            //           (json.decode(data['unCompleted']) as List)
-            //               .map((e) => Equation.fromJson(e))
-            //               .toList();
-            //       return Column(children: [
-            //         Text('unCompleted'),
-            //         for (final e in list)
-            //           Row(
-            //             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            //             children: [
-            //               Text('${e.firstOpeartor}'),
-            //               Text(e.opeartor),
-            //               Text('${e.secondOpeartor}'),
-            //               Text('='),
-            //               Text('${e.result}'),
-            //             ],
-            //           )
-            //       ]);
-            //     }
-            //     return Text('');
-            //   },
-            // ),
+    
             ElevatedButton(
               child: Text("First Equation"),
               onPressed: () {
                 FlutterBackgroundService().sendData({
-                  "action": "add",
+                  "action": addKey,
                   'equation': Equation(
-                          firstOpeartor: 2,
+                          firstOpeartor: 50000000,
                           secondOpeartor: 3,
                           opeartor: '+',
-                          time: DateTime.now().add(Duration(seconds: 10)))
+                          time: DateTime.now().add(Duration(seconds: 20)))
                       .toJson()
                 });
               },
@@ -224,7 +178,7 @@ class _MyAppState extends State<MyApp> {
               child: Text("Second Equation"),
               onPressed: () {
                 FlutterBackgroundService().sendData({
-                  "action": "add",
+                  "action": addKey,
                   'equation': Equation(
                           firstOpeartor: 122,
                           secondOpeartor: 3,
